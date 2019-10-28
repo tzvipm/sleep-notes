bd4123e14c886cf68f4c1412dbfd7620c96a4d30
Tzvi Melamed <tzvi@melamed.biz>
2019-10-28 01:06:18 +0000

    Add json tree view

---
+++ packages/sleep-notes/components/SleepTimeline.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SleepTimeline() {
+    return (
+        <div>Timeline coming soon</div>
+    );
+}
--- packages/sleep-notes/pages/index.tsx
+++ packages/sleep-notes/pages/index.tsx
@@ -10,6 +10,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import JSONTree from 'react-json-tree';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -27,11 +28,36 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         paper: {
             padding: theme.spacing(2),
-            textAlign: 'center',
             color: theme.palette.text.secondary,
+        },
+        paperform: {
+            padding: theme.spacing(2),
+            color: theme.palette.text.secondary,
+            textAlign: 'center',
         },
     }),
 );
+
+const jsonTheme = {
+  "scheme": "Material Lighter",
+  "author": "Nate Peterson",
+  "base00": "#FAFAFA",
+  "base01": "#E7EAEC",
+  "base02": "#CCEAE7",
+  "base03": "#CCD7DA",
+  "base04": "#8796B0",
+  "base05": "#80CBC4",
+  "base06": "#80CBC4",
+  "base07": "#FFFFFF",
+  "base08": "#FF5370",
+  "base09": "#F76D47",
+  "base0A": "#FFB62C",
+  "base0B": "#91B859",
+  "base0C": "#39ADB5",
+  "base0D": "#6182B8",
+  "base0E": "#7C4DFF",
+  "base0F": "#E53935",
+};
 
 export default function Home() {
     const classes = useStyles({});
@@ -69,7 +95,7 @@ export default function Home() {
 
             <Grid container spacing={3}>
                 <Grid item md={6} sm={12}>
-                    <Paper className={classes.paper}>
+                    <Paper className={classes.paperform}>
                         <p>Copy and paste your sleep note into the text field below:</p>
                         <TextField variant="outlined" value={note} onChange={evt => setNote(evt.target.value)} multiline fullWidth />
                         <Button className={classes.button} variant="contained" color="primary" onClick={handleProcess}>Process</Button>
@@ -77,7 +103,7 @@ export default function Home() {
                 </Grid>
                 <Grid item md={6} sm={12}>
                     <Paper className={classes.paper}>
-                        <pre>{JSON.stringify(tree)}</pre>
+                        <JSONTree data={tree} theme={jsonTheme} invertTheme={false} />
                     </Paper>
                 </Grid>
             </Grid>
